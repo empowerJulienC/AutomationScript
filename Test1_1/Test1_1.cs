8d8b9a5bcfefd08576d9341b8eaa7eabb3406214
jcaldana <jcaldana@LPT-VOOO-220049>
2023-03-28 16:37:27 +0200

    Order

--- Test1_1/Test1_1.cs
+++ Test1_1/Test1_1.cs
@@ -55,6 +55,7 @@ namespace Test1_1
 	using System.Collections.Generic;
 	using System.Globalization;
 	using System.Text;
+	using Empower.Library.Room3;
 	using Skyline.DataMiner.Automation;
 	using Skyline.DataMiner.Core.DataMinerSystem.Automation;
 
@@ -75,6 +76,9 @@ namespace Test1_1
 			{
 				engine.GenerateInformation(element.Name);
 			}
+
+			var order = OrderFactory.CreateOrder("Burger");
+			order.Dispatch("JC");
 		}
 	}
 }
